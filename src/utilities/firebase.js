55d2f5abfce92ec3531b8fd1bb780ca096b969a8
pizzapup <57334884+pizzapup@users.noreply.github.com>
2023-04-15 11:41:42 -0400

    Update firebase.js

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -1,11 +1,13 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyDIzdA23NE8c24S2GBhObPu3e9PAE3FCXc",
-  authDomain: "crud-todo-30019.firebaseapp.com",
-  projectId: "crud-todo-30019",
-  storageBucket: "crud-todo-30019.appspot.com",
-  messagingSenderId: "169310683772",
-  appId: "1:169310683772:web:1f57ffb083e854474e7ff9",
+  apiKey: "AIzaSyDzXN5Z3pKEEp3xcLJHNeh89r19wC2uONc",
+ authDomain: "n423-final-285cd.firebaseapp.com",
+  databaseURL: "https://n423-final-285cd-default-rtdb.firebaseio.com",
+  projectId: "n423-final-285cd",
+  storageBucket: "n423-final-285cd.appspot.com",
+  messagingSenderId: "153515000745",
+  appId: "1:153515000745:web:ebf06592c71e78f659fab8",
+  measurementId: "G-1QEZSQS9ZQ"
 };
 firebase.initializeApp(firebaseConfig);
 export default firebase;
